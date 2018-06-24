6d8bd0e1e3116790e768218e3d04641109281025
Manisha Jain <mona@manishas-macbook.local>
2018-06-24 16:12:29 +0530

    Convert to MVC Design pattern.

---
+++ Todoey/Model/item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Manisha Jain on 24/06/18.
+//  Copyright © 2018 Aashrit Garg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
